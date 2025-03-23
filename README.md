4d6abc9ea44ed51c8038aefe40f1fd5afbff5c55
Revath Sankar <37594493+rsankar23@users.noreply.github.com>
2025-03-23 10:21:59 -0400

    --finalization
    Added all files for final submission and execution

---
+++ README.md
@@ -0,0 +1,6 @@
+# RadiologistAI
+1. Run 'pip install - requirements.txt'
+2. Run the mri_model.ipynb file to completion to generate adv_big_weights.pkl file for Streamlit  application
+3. Run 'git clone https://github.com/deepseek-ai/DeepSeek-VL
+        cd DeepSeek-VL
+        pip install -e .' to get access to the DeepSeek Model locally.
--- rag.py
+++ rag.py
@@ -9,13 +9,22 @@ import faiss
 from sentence_transformers import SentenceTransformer
 from typing import Optional, TypedDict
 import torch
-
+from datasets import load_dataset, Image
 from deepseek_vl.models import VLChatProcessor, MultiModalityCausalLM
 from deepseek_vl.utils.io import load_pil_images
 import streamlit as st
 
-PDF_STORE = r"/Users/revathsankar/Documents/Adv Big AI/radiologyAI/Pub1564webNew-74666420.pdf"
+PDF_STORE = r"./Pub1564webNew-74666420.pdf"
 torch.classes.__path__ = [] # add this line to manually set it to empty. 
+TEST_DATASET = "benschill/brain-tumor-collection"
+
+
+
+def load_hf_dataset(split:str="train"):
+    df = load_dataset(TEST_DATASET, split=split, trust_remote_code=True)#.cast_column("image", Image(decode=True))
+    return df
+
+
 
 class Pipeline:
     def __init__(self):
@@ -31,20 +40,20 @@ class Pipeline:
             encode_kwargs=encode_kwargs
         )
 
-    def add_docs(self,):
-        loader = PyPDFLoader(PDF_STORE,
+    def add_docs(self, path):
+        loader = PyPDFLoader(path,
                             mode="page",)
                             #  images_inner_format='markdown-img',
                             #  images_parser=RapidOCRBlobParser())
         docs = loader.load()
-
+        st.chat_message("ai").write("Loading document...")
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=500,
             chunk_overlap=200,
             add_start_index=True
         )
         all_splits = text_splitter.split_documents(docs)
-        print(f"{len(all_splits)} chunks generated")
+        st.chat_message("ai").write(f"{len(all_splits)} chunks generated")
 
         embed_model = "BAAI/bge-large-en-v1.5"
         model = SentenceTransformer(embed_model)
@@ -61,15 +70,18 @@ class Pipeline:
         #NOTE: Using FAISS Model
         index = faiss.IndexFlatL2(len(embeddings.embed_query("what is the weather?")))
 
-        vector_store = FAISS(
+        self.vector_store = FAISS(
             embedding_function=embeddings,
             index=index,
             docstore=InMemoryDocstore(),
             index_to_docstore_id={},
         )
 
-
-        doc_ids = vector_store.add_documents(documents=all_splits)
+        st.chat_message("ai").write("Adding to vectorstore...")
+        doc_ids = self.vector_store.add_documents(documents=all_splits)
+        st.chat_message("ai").write("Saving to model...")
+        self.save_model()
+        return doc_ids
 
 
     def save_model(self):
@@ -94,6 +106,7 @@ class DeepSeek:
 
         self.vl_gpt: MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(self.model_path, trust_remote_code=True)
         self.vl_gpt = self.vl_gpt.to(torch.bfloat16).cpu().eval()
+        self.df = load_hf_dataset()
 
 
     def get_initial_analysis(self, img_path):
@@ -123,10 +136,24 @@ class DeepSeek:
             }
         ])
         return further_analysis
+    
+    def dataset_analyze(self, img_path):
+        initial_analysis = [
+            {
+                "role": "User",
+                "content": "<image_placeholder>Identify the tumor type in the image as either a meningioma, glioma, pituitary tumor or no tumor class. Do not provide any other information, however ensure that you think through the analysis, even plan steps for identification beforehand. Nothing else. Never provide an empty response; always provide some form of acknowledgement of the users request.",
+                "images": [f"{img_path}"]
+            },
+            {
+                "role": "Assistant",
+                "content": ""
+            }
+        ]
+        return initial_analysis
 
     def analyze_image(self, conversation:str):
         # load images and prepare for inputs
-        print("New thread spun!")
+        st.toast("New thread spun!")
         pil_images = load_pil_images(conversation)
         prepare_inputs = self.vl_chat_processor(
             conversations=conversation,
